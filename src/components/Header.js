499e62cad1ced0ad9429ed331a199d377fc1527e
Elshad <safarov.elshad007@gmail.com>
2019-04-08 14:08:28 +0500

    Added Stats method

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Stats from './Stats.js';
 
 const Header = function(props){
 	return (
 		<header>
+			<Stats todos={props.todos} />
 			<h1>{props.title}</h1>
 		</header>	
 	);
 };
 
 Header.propTypes = {
-	title: PropTypes.string.isRequired
+	title: PropTypes.string.isRequired,
+	todos: PropTypes.array.isRequired
 };
 
 export default Header;
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,2 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+const Stats = function (props){
+	const total = props.todos.length;
+	const completed = props.todos.filter((todo) => {
+		return todo.completed;
+	}).length;
+	const notCompleted = total - completed; 
+
+	return (
+		<table className="stats" >
+			<tbody>
+				<tr>
+					<th>Всего задач:</th>
+					<td>{total}</td>
+				</tr>
+				<tr>
+					<th>Выполнено:</th>
+					<td>{completed}</td>
+				</tr>
+				<tr>
+					<th>Осталось:</th>
+					<td>{notCompleted}</td>
+				</tr>
+			</tbody>
+		</table>
+	);
+}
+
+Stats.propTypes = {
+	todos: PropTypes	.array.isRequired
+};
+
+export default Stats;
