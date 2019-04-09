635be8b8413347900c8ed5b4b2e7ba01b78ccaca
Elshad <safarov.elshad007@gmail.com>
2019-04-09 15:35:50 +0500

    Added handleChange method in Form class

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,29 +8,44 @@ class Form extends React.Component {
 		super(props);
 
 		this.state = {
-			
+			title: ''
 		};
 
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
 
 	handleSubmit (evt){
 		evt.preventDefault();
-		const title = this.refs.title.value.trim();
+		const title = this.state.title.trim();
+		const input = evt.target.querySelector('input');
 		if(title && isNaN(title)){
+			input.classList.remove('inputError');
 			this.props.onAdd(title);
-			this.refs.title.classList.remove('inputError');
-			this.refs.title.value = '';
+			this.setState({
+				title: ''
+			});			
 		}else{
-			this.refs.title.classList.add('inputError');
+			input.classList.add('inputError');
 		}
+	}
+
+	handleChange (evt){
+		const title = event.target.value;
+		this.setState({
+			title: title
+		});
 	}
 
 	render (){
 		return (
 			<form className="todo-form" onSubmit={this.handleSubmit}>
-				<input type="text" ref="title" placeholder="Что нужно сделать?" />
-
+				<input 
+					type="text" 
+					value={this.state.title}
+					placeholder="Что нужно сделать?"
+					onChange={this.handleChange}
+				/>
 				<Button type="submit">Добавить</Button>
 			</form>
 		);
