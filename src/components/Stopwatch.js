618f49362c9c05b3ba4ee9502a016793b186e467
Elshad <safarov.elshad007@gmail.com>
2019-04-13 23:43:46 +0500

    Added Stopwatch class

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,2 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import Button from './Button.js';
+
+class Stopwatch extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state = {
+		
+		};
+	}
+
+	render(){
+		return (
+			<section className="stopwatch">
+				<div className="stopwatch-time">00:23</div>
+
+				<div className="stopwatch-controls">
+					<Button className="icon" icon="play_arrow" />
+					<Button className="icon" icon="stop" />
+				</div>
+			</section>
+		);
+	}
+}
+
+export default Stopwatch;
